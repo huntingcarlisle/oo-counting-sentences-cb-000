6adb98784cdac762253635f7c3736280dc4f2a3b
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-18 21:01:56 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,9 +16,16 @@ class String
 
   def count_sentences
     count = 0
+<<<<<<< HEAD
     array = self.split(/[?!.]/)
     array.each do |sentence| 
     if !(sentence == nil) && !(sentence == "")
+=======
+    array = []
+    array << self.split(/[?!.]/)
+    array.each do |sentence| 
+    if !sentence == nil || !sentence == "" 
+>>>>>>> 3e64e12833ce6c9a667726774deeb579b0c0e11d
       count += 1 
       end
     end
